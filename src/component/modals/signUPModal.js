0d28d99ad7e8668ce6aa7f181ed0ffcfc54e780d
ga0-c1028 <gayoung.rho@gmail.com>
2021-03-27 22:35:30 +0900

    [CHG]

--- src/component/modals/signUPModal.js
+++ src/component/modals/signUPModal.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Modal, Button, Form, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {GoogleLogin } from 'react-google-login';
 
 const SignUpModal = ({show, onHide}) => {
+
+    
     return (
         <Modal
             show={show}
@@ -18,24 +21,38 @@ const SignUpModal = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
             <Form>
-                <Form.Group controlId="formBasicName">
+                {/* <Form.Group controlId="formBasicName">
                     <Form.Label>이름</Form.Label>
                     <Form.Control placeholder="이름을 입력하세요" />
-                </Form.Group>
+                </Form.Group> */}
 
                 <Form.Group controlId="formBasicNickname">
                     <Form.Label>닉네임</Form.Label>
                     <Form.Control placeholder="닉네임을 입력하세요" />
                     <Form.Text className="text-muted">
-                    선택사항입니다.
+                    선
                     </Form.Text>
                 </Form.Group>
                 
+                <Form.Group controlId="formBasicWebMail">
+                    <Form.Label>웹메일</Form.Label>
+                    <Form.Control placeholder="웹메일/지메일(G-suite) 입력하세요" />
+                    <Form.Text className="text-muted">
+                    @hufs.ac.kr 앞 부분까지만 입력해주세요.
+                    <br/>위 웹메일로 학생 확인 인증 메일이 발송됩니다. 인증까지는 최대 24시간이 소요됩니다.
+                    </Form.Text>
+                </Form.Group>
+
                 {/* <Form.Label>전공</Form.Label> */}
-                <Form.Group controlId="formBasicMajor">
+                {/* <Form.Group controlId="formBasicMajor">
                     <Form.Label>1전공</Form.Label>
                     <Form.Control placeholder="1전공을 입력하세요" />
-                </Form.Group>
+                </Form.Group> */}
+                <Form.Control as="select">
+                    <Form.Label>1전공</Form.Label>
+                    <option></option>
+                    <option></option>
+                </Form.Control>
                 <Form.Group controlId="formBasicMinor">
                     <Form.Label>이중전공</Form.Label>
                     <Form.Control placeholder="이중전공/부전공을 입력하세요" />
@@ -44,20 +61,10 @@ const SignUpModal = ({show, onHide}) => {
                     전공 변경은 한 번만 가능합니다. 정확하게 입력해주세요.
                 </Form.Text>
                 
-
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>메일</Form.Label>
-                    <Form.Control type="email" placeholder="웹메일/지메일(G-suite) 입력하세요" />
-                    <Form.Text className="text-muted">
-                    위 메일로 인증 메일이 보내집니다. 인증까지는 최대 24시간이 소요됩니다.
-                    </Form.Text>
-                </Form.Group>
-                
                 <Button variant="primary" type="submit">
                     Sign Up
                 </Button>
                 </Form>
-            {/* <GoogleLogin /> */}
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={onHide}>Close</Button>
